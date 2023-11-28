838e55ba42babe4c44e05ba895bf07e703635f8e
PabloMejias7 <152323021+PabloMejias7@users.noreply.github.com>
2023-11-28 19:44:25 +0100

    Update Script.cs

--- Script.cs
+++ Script.cs
@@ -7,13 +7,13 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello World");
-        print(17);
+       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      print("crickets")  
     }
 }
